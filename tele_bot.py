4a42b8583e82e38131558d7df606b48fc557a8d2
Naveen Nammi <71389724+naveen-colab@users.noreply.github.com>
2020-09-19 15:38:14 +0530

    Update tele_bot.py

--- tele_bot.py
+++ tele_bot.py
@@ -9,13 +9,13 @@ def adafruit_auth(n):
     x = os.getenv("ADAFRUIT_IO_USERNAME")  #ADAFRUIT_IO_USERNAME
     y = os.getenv("ADAFRUIT_IO_KEY")    #ADAFRUIT_IO_KEY
     aio = Client(x,y)
-    if n==1:
-      test = aio.feeds('foo')
+    if n==1:            #light on condtion 
+      test = aio.feeds('light_status')
       aio.send_data(test.key, 10)
-    else:
-      test = aio.feeds('foo')
+    else:               #light off condition
+      test = aio.feeds('light_status')
       aio.send_data(test.key, 0)
-def light_on(bot,update):
+def light_on(bot,update):   #light on function
     str = "light on"
     print(str)
     adafruit_auth(1)
@@ -23,7 +23,7 @@ def light_on(bot,update):
     bot.send_message(chat_id,text=str)
     bot.send_photo(chat_id,photo="https://static.scientificamerican.com/sciam/cache/file/2B38DE31-C1D3-4339-8808D61972976EE4.jpg")
 
-def light_off(bot,update):
+def light_off(bot,update):      #light off function
       str = "light off"
       print(str)
       adafruit_auth(0)
@@ -31,12 +31,12 @@ def light_off(bot,update):
       bot.send_message(chat_id,text=str)
       bot.send_photo(chat_id,photo="https://ak.picdn.net/shutterstock/videos/2860753/thumb/1.jpg")
 
-tele_api = os.getenv("telegram_api")
+#tele_api = os.getenv("telegram_api")
 updater = Updater("1116113868:AAGpjuLnhS19HgkJ1W8R5zZahtghy8augV4")
 print("crossed the updater")
 dispatch = updater.dispatcher
-commandhandler1 = CommandHandler("off",light_off)
-commandhandler2 = CommandHandler("on",light_on)
+commandhandler1 = CommandHandler("light off",light_off)
+commandhandler2 = CommandHandler("light on",light_on)
 dispatch.add_handler(commandhandler1)
 dispatch.add_handler(commandhandler2)
 updater.start_polling()
